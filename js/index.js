6a10d9a8a73f267f39e5f90b2ec8ec60bcd3b211
Brian Zhou <brianhzhou@gmail.com>
2022-05-30 14:22:13 -0700

    - add pathExist function - adjust filter css

--- js/index.js
+++ js/index.js
@@ -1,40 +1,47 @@
 // returns a dictionary of HTTP parameters & values
 const getHttpParam = () => {
-    const siteUrl = window.location.href;
-    const paramStartPos = siteUrl.indexOf("?");
-    let result = {};
-    
-    if (paramStartPos == -1) return result;
-    
-    const paramString = siteUrl.slice( paramStartPos + 1 );
-    const paramArr = paramString.split("&");
-    
-    for (const parameter of paramArr) {
-      const sepPos = parameter.indexOf("=");
-      if(sepPos != -1) {
-        const keyStr = parameter.slice(0,sepPos);
-        const valueStr = parameter.slice(sepPos + 1);
-        
-        result[keyStr] = valueStr;
-      }
+  const siteUrl = window.location.href;
+  const paramStartPos = siteUrl.indexOf("?");
+  let result = {};
+  
+  if (paramStartPos == -1) return result;
+  
+  const paramStr = siteUrl.slice( paramStartPos + 1 );
+  const paramArr = paramStr.split("&");
+  
+  for (const parameter of paramArr) {
+    const sepPos = parameter.indexOf("=");
+    if(sepPos != -1) {
+      const keyStr = parameter.slice(0, sepPos);
+      const valueStr = parameter.slice(sepPos + 1);
+      
+      result[keyStr] = valueStr;
     }
-  
-    return result;
   }
-  
+
+  return result;
+}
+
+function pathExists(url, callback){
+  jQuery.ajax({
+    type: 'HEAD',
+    url: url,
+    success: () => callback(true),
+    error: () => callback(false)
+  });
+}
+
 // Get Json
 jQuery.getJSON("./info.json", data => {
     console.log(data);
 });
 
-
-// get Text file
-jQuery.get("./club-data/club-list.txt", data => {
+// get club list
+jQuery.get("./data/club-list", data => {
     const clubList = data.split("\r\n");
     console.log(clubList);
 });
 
-
-pathExists("./club-data/coding", (result) => {
+pathExists("./data/clubs/coding", (result) => {
     console.log("res: " + result);
 });
