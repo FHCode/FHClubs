6e06e692424802bb8a9820bf45489df900ea4436
Brian Zhou <brianhzhou@gmail.com>
2022-06-04 17:02:22 -0700

    header & footer fix attempt #3

--- js/main.js
+++ js/main.js
@@ -1,18 +1,2 @@
-function pathExists(url, callback){
-    jQuery.ajax({
-        type: 'HEAD',
-        url: url,
-        success: () => callback(true),
-        error: () => callback(false)
-    });
-}
-
-pathExists("./partials", bool = () => {
-    if(bool == true) {
-        $("#header-container").load("/partials/header.html");
-        $("#footer-container").load("/partials/footer.html");
-    } else {
-        $("#header-container").load("/FHClubs/partials/header.html");
-        $("#footer-container").load("/FHClubs/partials/footer.html");        
-    }
-});
+$("#header-container").load("../partials/header.html");
+$("#footer-container").load("../partials/footer.html");
